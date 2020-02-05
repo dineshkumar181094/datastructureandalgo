4b46cc9ec23df84c952b7ed0a3382314e1007652
dineshkumar181094 <36758667+dineshkumar181094@users.noreply.github.com>
2020-02-05 21:44:24 +0530

    Create binarytree.js

---
+++ binarytree.js
@@ -0,0 +1,61 @@
+
+class Node {
+  constructor(value) {
+  this.value = value
+  this.left = null
+  this.right = null
+  }
+}
+
+class BinarSearchTree {
+constructor () {
+  this.root = null
+}
+insert(value) {
+ let newNode = new Node(value)
+ if(this.root === null){
+   this.root = newNode
+   return this
+ } 
+ else {
+   let currentNode = this.root
+   while(true) {
+     if(value < currentNode.value) {
+       //takeleft 
+       if(currentNode.left === null) {
+         currentNode.left = newNode
+         return this
+       }
+       else {
+         currentNode = currentNode.left
+       }
+     }
+     else {
+       //take right
+       if (currentNode.right === null) {
+         currentNode.right = newNode
+         return this
+       }
+       else {
+         currentNode = currentNode.right
+       }
+
+     }
+   }
+   
+ }
+}
+lookup(value) {
+
+}
+}
+
+binartree = new BinarSearchTree()
+binartree.insert(9)
+binartree.insert(4)
+binartree.insert(6)
+binartree.insert(20)
+binartree.insert(170)
+binartree.insert(15)
+JSON.stringify(binartree.insert(1))
+
